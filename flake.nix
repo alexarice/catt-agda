04236596c514e8a29d1e3419ea917f1295f49e50
Alex Rice <alexrice999@hotmail.co.uk>
2023-08-18 13:31:01 +0100

    Add flake

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Formalisation of Catt with semistrictness in Agda";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    all-agda.url = "github:alexarice/all-agda";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, all-agda }: flake-utils.lib.eachDefaultSystem (system: let
+    pkgs = import nixpkgs { inherit system; overlays = [ all-agda.overlay.${system} ]; };
+  in {
+    test = 1;
+    devShell = pkgs.mkShell {
+      nativeBuildInputs = with pkgs; [
+        cabal-install
+        haskell-language-server
+        haskellPackages.fix-whitespace
+        (agdaPackages-2_6_3.agda2hs.withPackages (p: [ p.standard-library ]))
+        (agda-2_6_3.withPackages (p: [ p.standard-library ]))
+      ];
+    };
+  });
+}
